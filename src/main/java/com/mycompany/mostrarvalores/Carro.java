4e1b02df26cfb2819c489b31be79ade1a6a38a56
USER <USER@DESKTOP-97I3UFU>
2022-03-31 16:02:23 -0500

    Mostrar Valores en la consola

---
+++ src/main/java/com/mycompany/mostrarvalores/Carro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mostrarvalores;
+
+/**
+ *
+ * @author USER
+ */
+public class Carro {
+    
+    String modelo;
+    String marca;
+    String color;
+    int precio;
+    
+    public void vehiculo(){
+ 
+        System.out.println("modelo del vehiculo: "+modelo+ "marca: "+marca+ "color: "+color+ "precio: "+precio);
+}
+    
+    
+}
---
+++ src/main/java/com/mycompany/mostrarvalores/Inicio.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mostrarvalores;
+
+/**
+ *
+ * @author USER
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        
+    Carro carro = new Carro();
+    carro.marca = "Chevrolet";
+    carro.modelo = "Family";
+    carro.color = "Rojo";
+    carro.precio = 25000;
+    
+    carro.vehiculo();
+    
+    
+    }
+}
